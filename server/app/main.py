18e61611401d9219a780742affa4e4ee5ac5ac19
annapotr <potryasaeva.anna@mail.ru>
2022-01-07 05:05:55 +0300

    Update

--- server/app/main.py
+++ server/app/main.py
@@ -2,7 +2,7 @@ import os
 import certifi as certifi
 import pymongo as pymongo
 from random import seed
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI, Response
 from fastapi.middleware.cors import CORSMiddleware
 import msgpack
 from fastapi.responses import RedirectResponse
@@ -27,29 +27,26 @@ async def redirect():
     return RedirectResponse(url='/site/index.html')
 
 
-@app.websocket("/get_data")
-async def read_item(ws: WebSocket):
-    await ws.accept()
-    while True:
-        data = await ws.receive_bytes()
-        filters = msgpack.unpackb(data, raw=False)
-        people = db["people"].find({
-            'age': {'$gte': filters['age_left'], '$lte': filters['age_right']},
-            'sex': {'$in': filters['sex']},
-            'happy': {'$gte': filters['happy_left'], '$lte': filters['happy_right']},
-            'sad': {'$gte': filters['sad_left'], '$lte': filters['sad_right']},
-            'angry': {'$gte': filters['angry_left'], '$lte': filters['angry_right']},
-            'surprised': {'$gte': filters['surprised_left'], '$lte': filters['surprised_right']},
-            'afraid': {'$gte': filters['afraid_left'], '$lte': filters['afraid_right']},
-            'disgusted': {'$gte': filters['disgusted_left'], '$lte': filters['disgusted_right']},
-            'neutral': {'$gte': filters['neutral_left'], '$lte': filters['neutral_right']},
-            'race': {'$in': filters['race']}
-        })
-        answer = []
-        for p in people:
-            p['id'] = str(p['_id'])
-            p['_id'] = str(p['_id'])
-            answer.append(p)
-        answer = msgpack.packb(answer, use_bin_type=True)
-        await ws.send_bytes(answer)
+@app.post("/get_data", response_class=Response)
+async def get_data(data: Any = Body(..., media_type="text/plain")):
+    filters = msgpack.unpackb(data, raw=False)
+    people = db["people"].find({
+        'age': {'$gte': filters['age_left'], '$lte': filters['age_right']},
+        'sex': {'$in': filters['sex']},
+        'happy': {'$gte': filters['happy_left'], '$lte': filters['happy_right']},
+        'sad': {'$gte': filters['sad_left'], '$lte': filters['sad_right']},
+        'angry': {'$gte': filters['angry_left'], '$lte': filters['angry_right']},
+        'surprised': {'$gte': filters['surprised_left'], '$lte': filters['surprised_right']},
+        'afraid': {'$gte': filters['afraid_left'], '$lte': filters['afraid_right']},
+        'disgusted': {'$gte': filters['disgusted_left'], '$lte': filters['disgusted_right']},
+        'neutral': {'$gte': filters['neutral_left'], '$lte': filters['neutral_right']},
+        'race': {'$in': filters['race']}
+    })
+    answer = []
+    for p in people:
+        p['id'] = str(p['_id'])
+        p['_id'] = str(p['_id'])
+        answer.append(p)
+    answer = msgpack.packb(answer, use_bin_type=True)
+    return answer
 
